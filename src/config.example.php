00e661e1e66c24cc36c068cd7ef1f7049459043b
leonid svetlichny <ls@webberry.ru>
2024-05-25 00:41:57 +0300

    Micro fixed and composer.json up

--- src/config.example.php
+++ src/config.example.php
@@ -1,6 +1,6 @@
 <?php return [
 	"key" => "-- YOUR API_KEY HERE --",
 	"signKey" => '-- YOUR SIGN KEY HERE --',
-	"host" => "API ENDPOINT HOST WITH PORT " #  e.g. 158.160.151.119:4000,
+	"host" => "API ENDPOINT HOST WITH PORT ", #  e.g. 158.160.151.119:4000,
 	"ssl" => false # Use or not ssl for connection
 ];

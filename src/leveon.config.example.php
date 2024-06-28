e0732b037bc1826f1154693de1e73ac97941fc6e
leonid svetlichny <ls@webberry.ru>
2024-06-28 12:16:19 +0300

    Micro fixed and composer.json up

--- src/leveon.config.example.php
+++ src/leveon.config.example.php
@@ -3,5 +3,5 @@
 	"signKey" => '-- YOUR SIGN KEY HERE --',
 	"host" => "api.cds.leveon.ru", # api host, default: api.cds.leveon.ru
 	"ssl" => true, # Use or not ssl for connection, default: true
-    "db" => "@/data/connector.sqlite", # @ is a placeholder for project root dir
+    "db" => "@/data/leveon/connector.sqlite", # @ is a placeholder for project root dir
 ];
